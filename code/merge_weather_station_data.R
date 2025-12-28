de6792577762353d56f3a4ca9584941be0f7b699
sbegmanov <sbegmanov@gmail.com>
2025-12-28 13:58:24 +0500

    merge weather stations

--- code/merge_weather_station_data.R
+++ code/merge_weather_station_data.R
@@ -11,8 +11,8 @@ station_data <- read_tsv("data/gchnd_regions_years.tsv")
 lat_long_prcp <- inner_join(prcp_data, station_data, by = "id") |> 
 filter((year != first_year & year != last_year) | year == 2025) |> 
 group_by(latitude, longtitude, year) |>
-summarize(mean_prcp = mean(prcp)) |>
-summarize(n = n())
+summarize(mean_prcp = mean(prcp))
+# |> summarize(n = n())
 
-lat_long_prcp |> ggplot(aes(x = n)) + geom_histogram()
-lat_long_prcp |> filter(n >= 100)
+# lat_long_prcp |> ggplot(aes(x = n)) + geom_histogram()
+# lat_long_prcp |> filter(n >= 100)
